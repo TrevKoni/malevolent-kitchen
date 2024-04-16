16dc2f4fc03e11c3f60ecfe8df4c4430355af234
TrevKoni <konitrev@gmail.com>
2024-04-16 23:02:36 +0200

    Created html for pages. Updated the navbar links.

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,9 +4,9 @@ import "../style/navbar.css";
 document.querySelector("#navbar").innerHTML = `
   <div class="nav-container">
     <a href="/" class="nav-link">Home</a>
-    <a href="./recipes.html" class="nav-link">Recipes</a>
-    <a href="./categories.html" class="nav-link">Categories</a>
-    <a href="./about.html" class="nav-link">About</a>
-    <a href="./contact.html" class="nav-link">Contact</a>
+    <a href="./recipes" class="nav-link">Recipes</a>
+    <a href="./categories" class="nav-link">Categories</a>
+    <a href="./about" class="nav-link">About</a>
+    <a href="./contact" class="nav-link">Contact</a>
   </div>
 `;
