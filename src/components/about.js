7e42cc96f357582e817804ab4875748419a68548
KristiKogami <fridakule2@gmail.com>
2024-04-16 16:08:48 +0200

    Added About page and its components

---
+++ src/components/about.js
@@ -0,0 +1,17 @@
+import "../../style.css";
+import "../style/about.css";
+
+document.querySelector('#about').innerHTML = `
+
+    <div class="about-container">
+        <div class="about-content">
+            <img src="/malevolent-kitchen.png" alt="Site Logo" class="about-content-image">
+            <h2 class="about-header">About</h2>
+            <h3 class="about-1">About the Website: </h3>
+            <p class="about-web">Malevolent Kitchen is <!-- Shto textin --> </p>
+            <h3 class="about-2">About Us: </h3>
+            <p class="about-us">We are <!-- Shto textin --> </p>
+        </div>
+    </div>
+
+`;
